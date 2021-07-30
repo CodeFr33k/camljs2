d739cc94420ea7c6d38e5f1c682d06021d84b940
CodeFr33k <noreply>
2021-07-30 11:34:26 +0930

    read css widths

--- readRecordFromText.ts
+++ readRecordFromText.ts
@@ -21,12 +21,16 @@ export default (chan) => {
                 const created = /created = (.*)/.exec(recordAsText);
                 const token = /token = (.*)/.exec(recordAsText);
                 const image = /@image = (.*)/.exec(recordAsText);
+                const width = /@width = (.*)/.exec(recordAsText);
+                const maxWidth = /@max-width = (.*)/.exec(recordAsText);
                 const record = {
                     lines,
                     username: username && username[1],
                     created: created && created[1],
                     token: token && token[1],
                     image: image && image[1],
+                    width: width && width[1],
+                    maxWidth: maxWidth && maxWidth[1],
                 };
                 yield csp.put(result, record); 
             }
